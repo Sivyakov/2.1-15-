9c9781b7f397d14ca205ee829f10d48361c575aa
сивяков никит <reus-kit@mail.ru>
2021-12-23 16:26:06 +0600

    Добавьте файлы проекта.

---
+++ 2.1 15в/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _2._1_15в
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.1 средний уровень 15в
+            Console.WriteLine("Введите бал за физику:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите бал за математику:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите бал за информатику:");
+            int c = int.Parse(Console.ReadLine());
+            if( (a>=4)&& (b>=4)&&(c>=4))
+            {
+                Console.WriteLine("Студент получит стипендию:");
+            }
+            else
+            {
+                Console.WriteLine("Студент не получит стипендию:");
+            }
+        }
+    }
+}
